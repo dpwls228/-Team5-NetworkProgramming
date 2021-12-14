b4c0f05130140de7d7fe3c5a47caea95611650cf
dpwls228 <84014910+dpwls228@users.noreply.github.com>
2021-12-15 02:09:03 +0900

    Update game_server.java

--- src/game_server.java
+++ src/game_server.java
@@ -1,12 +1,30 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.Vector;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class game_server {
-
+	
+	Vector<game_thread> alluser;		//연결된 모든 클라이언트
+	Vector<game_thread> waituser;	//대기실에 있는 클라이언트
+	Vector<game_room> room;			//생성된 Room
+	
 	public static void main(String[] args) {
+		game_server server = new game_server();
 		ServerSocket listener = null;
-
+		
+		server.alluser = new Vector<>();
+		server.waituser = new Vector<>();
+		server.room = new Vector<>();
 		Scanner inputstream = null;
 
 		int nPort = 0;
@@ -28,7 +46,7 @@ public class game_server {
 					System.out.println("Waitting Clients...\n");
 					Socket client = listener.accept();
 					System.out.println("One client connected! \n");
-					game_thread user = new game_thread(client);
+					game_thread user = new game_thread(client, server);
 					user.start();
 				} catch (Exception e) {
 
